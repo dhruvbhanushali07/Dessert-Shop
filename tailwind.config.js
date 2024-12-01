d6759e63592ba1ce90b5aa3c64e2988321c44e54
Dhruv Bhanushali <dhruvbhanushali0007@gmail.com>
2024-12-01 22:51:02 +0530

    modified project colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      Red: "hsl(14, 86%, 42%)",
+      Green: "hsl(159, 69%, 38%)",
+      Rose: {
+        50: "hsl(20, 50%, 98%)",
+        100: "hsl(13, 31%, 94%)",
+        300: "hsl(14, 25%, 72%)",
+        400: "hsl(7, 20%, 60%)",
+        500: "hsl(12, 20%, 44%)",
+        900: " hsl(14, 65%, 9%)",
+      },
+    },
     extend: {},
   },
   plugins: [],
-}
+};
