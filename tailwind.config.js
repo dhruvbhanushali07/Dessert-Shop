36cb9f085fc7fc13d9cca3059113326ecdb1049d
Dhruv Bhanushali <dhruvbhanushali0007@gmail.com>
2024-12-10 23:12:27 +0530

    extended colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      "Red-primary": "hsl(14, 86%, 42%)",
-      Green: "hsl(159, 69%, 38%)",
-      Rose: {
-        50: "hsl(20, 50%, 98%)",
-        100: "hsl(13, 31%, 94%)",
-        300: "hsl(14, 25%, 72%)",
-        400: "hsl(7, 20%, 60%)",
-        500: "hsl(12, 20%, 44%)",
-        900: " hsl(14, 65%, 9%)",
+    fontFamily: {
+      "Red-Hat-Text": ["Red Hat Text"],
+    },
+    extend: {
+      colors: {
+        "Red-primary": "hsl(14, 86%, 42%)",
+        Green: "hsl(159, 69%, 38%)",
+        Rose: {
+          50: "hsl(20, 50%, 98%)",
+          100: "hsl(13, 31%, 94%)",
+          300: "hsl(14, 25%, 72%)",
+          400: "hsl(7, 20%, 60%)",
+          500: "hsl(12, 20%, 44%)",
+          900: " hsl(14, 65%, 9%)",
+        },
       },
     },
-    fontFamily:{
-      "Red-Hat-Text": ['Red Hat Text']
-    },
-    extend: {},
   },
   plugins: [],
 };
